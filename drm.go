ebc528b5b8967659a760f4d7a99cc969e413d257
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2017-04-22 21:08:34 -0300

    almost working :-)
    Signed-off-by: Tiago Natel de Moura <tiago.natel@neoway.com.br>

--- drm.go
+++ drm.go
@@ -3,27 +3,25 @@ package drm
 import (
 	"fmt"
 	"os"
-	"syscall"
 	"unsafe"
+
+	"github.com/tiago4orion/drm/ioctl"
 )
 
 //
 // Driver version information.
 //
 // drm.GetVersion() and drm.SetVersion().
-// dowsn't works.. requires cgo
-// type Version = C.struct_drmversion
-// :(
 type Version struct {
-	Major      int            // Major version
-	Minor      int            // Minor version
-	PatchLevel int            // Patch level
-	NameLen    int            // Length of name buffer
-	Name       unsafe.Pointer // Name of driver
-	DateLen    int            // Length of date buffer
-	date       unsafe.Pointer // User-space buffer to hold date
-	DescLen    int            // Length of desc buffer
-	Desc       unsafe.Pointer // User-space buffer to hold desc
+	Major   int32
+	Minor   int32
+	Patch   int32
+	Namelen int64
+	Name    unsafe.Pointer
+	Datelen int64
+	Date    unsafe.Pointer
+	Desclen int64
+	Desc    unsafe.Pointer
 }
 
 const (
@@ -32,38 +30,10 @@ const (
 	Render
 )
 
-const (
-	_IOC_NONE  = 0x0
-	_IOC_WRITE = 0x1
-	_IOC_READ  = 0x2
-
-	_IOC_NRBITS   = 8
-	_IOC_TYPEBITS = 8
-	_IOC_SIZEBITS = 14
-	_IOC_DIRBITS  = 2
-	_IOC_NRSHIFT  = 0
-
-	_IOC_TYPESHIFT = _IOC_NRSHIFT + _IOC_NRBITS
-	_IOC_SIZESHIFT = _IOC_TYPESHIFT + _IOC_TYPEBITS
-	_IOC_DIRSHIFT  = _IOC_SIZESHIFT + _IOC_SIZEBITS
-)
-
 var (
-	IOCTLVersion = uintptr(_IOWR('d', 0x00, int(unsafe.Sizeof(Version{}))))
+	v               Version
+	IOCTLVersion, _ = ioctl.NewCode(ioctl.Read, uint16(unsafe.Sizeof(v)), 'd', 0)
 )
-
-func _IOC(dir int, t int, nr int, size int) int {
-	return (dir << _IOC_DIRSHIFT) | (t << _IOC_TYPESHIFT) |
-		(nr << _IOC_NRSHIFT) | (size << _IOC_SIZESHIFT)
-}
-
-func _IOR(t int, nr int, size int) int {
-	return _IOC(_IOC_READ, t, nr, size)
-}
-
-func _IOWR(t int, nr int, size int) int {
-	return _IOC(_IOC_READ|_IOC_WRITE, t, nr, size)
-}
 
 func Available() (*Version, error) {
 	f, err := openMinor(0, Primary)
@@ -98,18 +68,31 @@ func openMinor(minor int, typ int) (*os.File, error) {
 }
 
 func GetVersion(file *os.File) (*Version, error) {
-	var version Version
-	err := ioctl(uintptr(file.Fd()), IOCTLVersion, uintptr(unsafe.Pointer(&version)))
+	version := &Version{}
+
+	err := ioctl.Do(uintptr(file.Fd()), uintptr(IOCTLVersion),
+		uintptr(unsafe.Pointer(version)))
 	if err != nil {
 		return nil, err
 	}
-	return &version, nil
-}
-
-func ioctl(fd, cmd, ptr uintptr) error {
-	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, cmd, ptr)
-	if err != 0 {
-		return err
+	if version.Namelen > 0 {
+		name := make([]byte, version.Namelen+1)
+		version.Name = unsafe.Pointer(&name[0])
 	}
-	return nil
+	if version.Datelen > 0 {
+		var date []byte = make([]byte, version.Datelen+1)
+
+		version.Date = unsafe.Pointer(&date[0])
+	}
+	if version.Desclen > 0 {
+		var desc []byte = make([]byte, version.Desclen+1)
+		version.Desc = unsafe.Pointer(&desc[0])
+	}
+	err = ioctl.Do(uintptr(file.Fd()), uintptr(IOCTLVersion),
+		uintptr(unsafe.Pointer(version)))
+	if err != nil {
+		return nil, err
+	}
+
+	return version, nil
 }
