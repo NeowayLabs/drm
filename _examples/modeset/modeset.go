0e0be6498721b9797350949cf6808bf4fd2fd5d9
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2017-05-02 00:06:23 -0300

    almost complete
    Signed-off-by: Tiago Natel de Moura <tiago.natel@neoway.com.br>

--- _examples/modeset/modeset.go
+++ _examples/modeset/modeset.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 
+	"launchpad.net/gommap"
+
 	"github.com/tiago4orion/drm"
 	"github.com/tiago4orion/drm/mode"
 )
@@ -14,18 +16,18 @@ import (
 type modeset struct {
 	width, height uint16
 	stride        uint32
-	size          uint32
+	size          uint64
 	handle        uint32
 	data          []byte
 
-	mode mode.Info
-	fb   uint32
-	conn uint32
-	crtc uint32
-	//	savedCtrc *drm.ModeCrtc
+	mode      mode.Info
+	fb        uint32
+	conn      uint32
+	crtc      uint32
+	savedCtrc *mode.Crtc
 }
 
-var modesetlist []modeset
+var modesetlist []*modeset
 
 func prepare(file *os.File) error {
 	res, err := mode.GetResources(file)
@@ -50,6 +52,7 @@ func prepare(file *os.File) error {
 			continue
 		}
 
+		modesetlist = append(modesetlist, dev)
 		fmt.Printf("%#v\n", conn)
 	}
 
@@ -72,11 +75,104 @@ func setupDev(file *os.File, res *mode.Resources, conn *mode.Connector, dev *mod
 	dev.height = conn.Modes[0].Vdisplay
 
 	log.Println("mode for connector %u is %ux%u\n", conn.ID, dev.width, dev.height)
+
+	err := findCrtc(file, res, conn, dev)
+	if err != nil {
+		return false, fmt.Errorf("no valid crtc for connector %u: %s", conn.ID, err.Error())
+	}
 	return true, nil
 }
 
-func findCrtc(file *os.File, res *mode.Resources, conn *mode.Connector, dev *modeset) {
-	// TODO
+func findCrtc(file *os.File, res *mode.Resources, conn *mode.Connector, dev *modeset) error {
+	var (
+		encoder *mode.Encoder
+		err     error
+	)
+
+	if conn.EncoderID != 0 {
+		encoder, err = mode.GetEncoder(file, conn.EncoderID)
+		if err != nil {
+			return err
+		}
+	}
+
+	if encoder != nil {
+		if encoder.CrtcID != 0 {
+			crtcid := encoder.CrtcID
+			found := false
+
+			for i := 0; i < len(modesetlist); i++ {
+				if modesetlist[i].crtc == crtcid {
+					found = true
+					break
+				}
+			}
+
+			if crtcid >= 0 && !found {
+				dev.crtc = crtcid
+				return nil
+			}
+		}
+	}
+
+	// If the connector is not currently bound to an encoder or if the
+	// encoder+crtc is already used by another connector (actually unlikely
+	// but lets be safe), iterate all other available encoders to find a
+	// matching CRTC.
+	for i := 0; i < len(conn.Encoders); i++ {
+		encoder, err := mode.GetEncoder(file, conn.Encoders[i])
+		if err != nil {
+			return fmt.Errorf("Cannot retrieve encoder: %s", err.Error())
+		}
+		// iterate all global CRTCs
+		for j := 0; j < len(res.Crtcs); j++ {
+			// check whether this CRTC works with the encoder
+			if (encoder.PossibleCrtcs & (1 << uint(j))) != 0 {
+				continue
+			}
+
+			// check that no other device already uses this CRTC
+			crtcid := res.Crtcs[j]
+			found := false
+			for k := 0; k < len(modesetlist); k++ {
+				if modesetlist[k].crtc == crtcid {
+					found = true
+					break
+				}
+			}
+
+			// we have found a CRTC, so save it and return
+			if crtcid >= 0 && !found {
+				dev.crtc = crtcid
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf("Cannot find a suitable CRTC for connector %d", conn.ID)
+}
+
+func createFramebuffer(file *os.File, dev *modeset) error {
+	fb, err := mode.CreateFB(file, dev.width, dev.height, 32)
+	if err != nil {
+		return fmt.Errorf("Failed to create framebuffer: %s", err.Error())
+	}
+	dev.stride = fb.Pitch
+	dev.size = fb.Size
+	dev.handle = fb.Handle
+	fbID, err := mode.AddFB(file, dev.width, dev.height, 24, 32, dev.stride, dev.handle)
+	if err != nil {
+		return fmt.Errorf("Cannot create dumb buffer: %s", err.Error())
+	}
+	dev.fb = fbID
+	mmap, err := gommap.Map(file.Fd(), gommap.PROT_READ|gommap.PROT_WRITE, gommap.MAP_SHARED)
+	if err != nil {
+		return fmt.Errorf("Failed to mmap framebuffer: %s", err.Error())
+	}
+	for i := uint64(0); i < dev.size; i++ {
+		mmap[i] = 0
+	}
+	return nil
 }
 
 func main() {
@@ -90,10 +186,25 @@ func main() {
 		fmt.Printf("drm device does not support dumb buffers")
 		return
 	}
-
 	err = prepare(file)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
 		return
+	}
+
+	for i := 0; i < len(modesetlist); i++ {
+		var err error
+
+		mset := modesetlist[i]
+		mset.savedCtrc, err = mode.GetCrtc(file, mset.crtc)
+		if err != nil {
+			log.Printf("[error] 1- Cannot set CRTC for connector %d", mset.conn)
+			return
+		}
+		err = mode.SetCrtc(file, mset.crtc, mset.fb, 0, 0, &mset.conn, 1, &mset.mode)
+		if err != nil {
+			log.Printf("[error] Cannot set CRTC for connector %d", mset.conn)
+			return
+		}
 	}
 }
--- mode/mode.go
+++ mode/mode.go
@@ -53,6 +53,16 @@ type (
 		subpixel          uint32
 	}
 
+	sysGetEncoder struct {
+		id  uint32
+		typ uint32
+
+		crtcID uint32
+
+		possibleCrtcs  uint32
+		possibleClones uint32
+	}
+
 	Info struct {
 		Clock                                         uint32
 		Hdisplay, HsyncStart, HsyncEnd, Htotal, Hskew uint16
@@ -92,6 +102,84 @@ type (
 
 		Encoders []uint32
 	}
+
+	Encoder struct {
+		ID   uint32
+		Type uint32
+
+		CrtcID uint32
+
+		PossibleCrtcs  uint32
+		PossibleClones uint32
+	}
+
+	sysCreateDumb struct {
+		height, width uint32
+		bpp           uint32
+		flags         uint32
+
+		// returned values
+		handle uint32
+		pitch  uint32
+		size   uint64
+	}
+
+	sysDestroyDumb struct {
+		handle uint32
+	}
+
+	sysMapDumb struct {
+		handle uint32 // Handle for the object being mapped
+		pad    uint32
+
+		// Fake offset to use for subsequent mmap call
+		// This is a fixed-size type for 32/64 compatibility.
+		offset uint64
+	}
+
+	sysFBCmd struct {
+		fbID          uint32
+		width, height uint32
+		pitch         uint32
+		bpp           uint32
+		depth         uint32
+
+		/* driver specific handle */
+		handle uint32
+	}
+
+	sysCrtc struct {
+		setConnectorsPtr uintptr
+		countConnectors  uint32
+
+		id   uint32
+		fbID uint32 // Id of framebuffer
+
+		x, y uint32 // Position on the frameuffer
+
+		gammaSize uint32
+		modeValid uint32
+		mode      Info
+	}
+
+	Crtc struct {
+		ID       uint32
+		BufferID uint32 // FB id to connect to 0 = disconnect
+
+		X, Y          uint32 // Position on the framebuffer
+		Width, Height uint32
+		ModeValid     int
+		Mode          Info
+
+		GammaSize int // Number of gamma stops
+	}
+
+	FB struct {
+		Height, Width, BPP, Flags uint32
+		Handle                    uint32
+		Pitch                     uint32
+		Size                      uint64
+	}
 )
 
 var (
@@ -99,9 +187,33 @@ var (
 	IOCTLModeResources = ioctl.NewCode(ioctl.Read|ioctl.Write,
 		uint16(unsafe.Sizeof(sysResources{})), drm.IOCTLBase, 0xA0)
 
+	// DRM_IOWR(0xA1, struct drm_mode_crtc)
+	IOCTLModeGetCrtc = ioctl.NewCode(ioctl.Read|ioctl.Write,
+		uint16(unsafe.Sizeof(sysCrtc{})), drm.IOCTLBase, 0xA1)
+
+	// DRM_IOWR(0xA2, struct drm_mode_crtc)
+	IOCTLModeSetCrtc = ioctl.NewCode(ioctl.Read|ioctl.Write,
+		uint16(unsafe.Sizeof(sysCrtc{})), drm.IOCTLBase, 0xA2)
+
+	// DRM_IOWR(0xA6, struct drm_mode_get_encoder)
+	IOCTLModeGetEncoder = ioctl.NewCode(ioctl.Read|ioctl.Write,
+		uint16(unsafe.Sizeof(sysGetEncoder{})), drm.IOCTLBase, 0xA6)
+
 	// DRM_IOWR(0xA7, struct drm_mode_get_connector)
 	IOCTLModeGetConnector = ioctl.NewCode(ioctl.Read|ioctl.Write,
 		uint16(unsafe.Sizeof(sysGetConnector{})), drm.IOCTLBase, 0xA7)
+
+	// DRM_IOWR(0xAE, struct drm_mode_fb_cmd)
+	IOCTLModeAddFB = ioctl.NewCode(ioctl.Read|ioctl.Write,
+		uint16(unsafe.Sizeof(sysFBCmd{})), drm.IOCTLBase, 0xAE)
+
+	// DRM_IOWR(0xB2, struct drm_mode_create_dumb)
+	IOCTLModeCreateDumb = ioctl.NewCode(ioctl.Read|ioctl.Write,
+		uint16(unsafe.Sizeof(sysCreateDumb{})), drm.IOCTLBase, 0xB2)
+
+	// DRM_IOWR(0xB3, struct drm_mode_map_dumb)
+	IOCTLModeMapDumb = ioctl.NewCode(ioctl.Read|ioctl.Write,
+		uint16(unsafe.Sizeof(sysMapDumb{})), drm.IOCTLBase, 0xB3)
 )
 
 func GetResources(file *os.File) (*Resources, error) {
@@ -215,4 +327,112 @@ func GetConnector(file *os.File, connid uint32) (*Connector, error) {
 	copy(ret.Encoders, encoders)
 
 	return ret, nil
+}
+
+func GetEncoder(file *os.File, id uint32) (*Encoder, error) {
+	encoder := &sysGetEncoder{}
+	encoder.id = id
+
+	err := ioctl.Do(uintptr(file.Fd()), uintptr(IOCTLModeGetEncoder),
+		uintptr(unsafe.Pointer(encoder)))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Encoder{
+		ID:             encoder.id,
+		CrtcID:         encoder.crtcID,
+		Type:           encoder.typ,
+		PossibleCrtcs:  encoder.possibleCrtcs,
+		PossibleClones: encoder.possibleClones,
+	}, nil
+}
+
+func CreateFB(file *os.File, width, height uint16, bpp uint32) (*FB, error) {
+	fb := &sysCreateDumb{}
+	fb.width = uint32(width)
+	fb.height = uint32(height)
+	fb.bpp = bpp
+	err := ioctl.Do(uintptr(file.Fd()), uintptr(IOCTLModeCreateDumb),
+		uintptr(unsafe.Pointer(fb)))
+	if err != nil {
+		return nil, err
+	}
+	return &FB{
+		Height: fb.height,
+		Width:  fb.width,
+		BPP:    fb.bpp,
+		Handle: fb.handle,
+		Pitch:  fb.pitch,
+		Size:   fb.size,
+	}, nil
+}
+
+func AddFB(file *os.File, width, height uint16,
+	depth, bpp uint8, pitch, boHandle uint32) (uint32, error) {
+	f := &sysFBCmd{}
+	f.width = uint32(width)
+	f.height = uint32(height)
+	f.pitch = pitch
+	f.bpp = uint32(bpp)
+	f.depth = uint32(depth)
+	f.handle = boHandle
+	err := ioctl.Do(uintptr(file.Fd()), uintptr(IOCTLModeAddFB),
+		uintptr(unsafe.Pointer(f)))
+	if err != nil {
+		return 0, err
+	}
+	return f.fbID, nil
+}
+
+func MapDumb(file *os.File, boHandle uint32) (uint64, error) {
+	mreq := &sysMapDumb{}
+	mreq.handle = boHandle
+	err := ioctl.Do(uintptr(file.Fd()), uintptr(IOCTLModeMapDumb),
+		uintptr(unsafe.Pointer(mreq)))
+	if err != nil {
+		return 0, err
+	}
+	return mreq.offset, nil
+}
+
+func GetCrtc(file *os.File, id uint32) (*Crtc, error) {
+	crtc := &sysCrtc{}
+	crtc.id = id
+	err := ioctl.Do(uintptr(file.Fd()), uintptr(IOCTLModeGetCrtc),
+		uintptr(unsafe.Pointer(crtc)))
+	if err != nil {
+		return nil, err
+	}
+	ret := &Crtc{
+		ID:        crtc.id,
+		X:         crtc.x,
+		Y:         crtc.y,
+		ModeValid: int(crtc.modeValid),
+		BufferID:  crtc.fbID,
+		GammaSize: int(crtc.gammaSize),
+	}
+
+	ret.Mode = crtc.mode
+	ret.Width = uint32(crtc.mode.Hdisplay)
+	ret.Height = uint32(crtc.mode.Vdisplay)
+	return ret, nil
+}
+
+func SetCrtc(file *os.File, crtcid, bufferid, x, y uint32, connectors *uint32, count int, mode *Info) error {
+	crtc := &sysCrtc{}
+	crtc.x = x
+	crtc.y = y
+	crtc.id = crtcid
+	crtc.fbID = bufferid
+	if connectors != nil {
+		crtc.setConnectorsPtr = uintptr(unsafe.Pointer(connectors))
+	}
+	crtc.countConnectors = uint32(count)
+	if mode != nil {
+		crtc.mode = *mode
+		crtc.modeValid = 1
+	}
+	return ioctl.Do(uintptr(file.Fd()), uintptr(IOCTLModeSetCrtc),
+		uintptr(unsafe.Pointer(crtc)))
 }
