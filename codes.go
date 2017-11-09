0ac181c24d072ee449e5ac8cbaff18413cfe4fc4
Valentin Hăloiu <vially.ichb@gmail.com>
2017-11-09 22:51:38 +0100

    Fix IOCTLVersion code

--- codes.go
+++ codes.go
@@ -9,8 +9,8 @@ import (
 const IOCTLBase = 'd'
 
 var (
-	// DRM_IOR(0x0, struct drm_version)
-	IOCTLVersion = ioctl.NewCode(ioctl.Read,
+	// DRM_IOWR(0x00, struct drm_version)
+	IOCTLVersion = ioctl.NewCode(ioctl.Read|ioctl.Write,
 		uint16(unsafe.Sizeof(version{})), IOCTLBase, 0)
 
 	// DRM_IOWR(0x0c, struct drm_get_cap)
